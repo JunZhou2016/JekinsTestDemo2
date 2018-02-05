dbfba19130a3722161bf8dd87fc7264f0f03a62e
zhoujun <zhoujun1@icerno.com>
2018-02-05 11:03:32 +0800

    JenkinsTestDemo3

---
+++ src/main/java/com/example/demo/JekinsTestDemo3Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsTestDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekinsTestDemo3Application.class, args);
+	}
+}
